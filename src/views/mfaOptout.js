a37a9e84e47e2c9982e0b91ce9d46edd12299bce
lleadbet <5595530+lleadbet@users.noreply.github.com>
2022-01-31 10:15:33 -0500

    fixing hardcoded values

--- src/views/mfaOptout.js
+++ src/views/mfaOptout.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from "react";
 
 import { useAuth0 } from "@auth0/auth0-react";
 import Loading from "../components/Loading";
+import { getConfig } from "../config";
 
 export const MFAOptoutPage = () => {
     const { getAccessTokenWithPopup } = useAuth0();
     const [accessToken, setAccessToken] = useState('')
     const [isFinished, setFinishedState] = useState('')
+    const config = getConfig()
+
 
     useEffect(()=>{
         if(!accessToken){
@@ -18,7 +21,7 @@ export const MFAOptoutPage = () => {
     const getAccessToken = async () => {
         let t = await getAccessTokenWithPopup({
             acr_values: 'http://schemas.openid.net/pape/policies/2007/06/multi-factor', 
-            audience: 'https://lucasleadbetter-sandbox.us.auth0.com/mfa/', 
+            audience: `https://${config.domain}/mfa/`, 
             scope:'read:authenticators remove:authenticators',
             redirectUri:`${window.location.origin}/mfa-out`
         })
@@ -27,12 +30,12 @@ export const MFAOptoutPage = () => {
     }
 
     const getMFAEnrollmentsAndDelete = async (token) => {
-       const res = await fetch(`https://lucasleadbetter-sandbox.us.auth0.com/mfa/authenticators`, {headers:{Authorization: `Bearer ${token}`}})
+       const res = await fetch(`https://${config.domain}/mfa/authenticators`, {headers:{Authorization: `Bearer ${token}`}})
        const mfaEnrollments = await res.json()
        console.log(mfaEnrollments)
 
        for(let mfa of mfaEnrollments){
-        let res = await fetch(`https://lucasleadbetter-sandbox.us.auth0.com/mfa/authenticators/${mfa.id}`, {
+        let res = await fetch(`https://${config.domain}/mfa/authenticators/${mfa.id}`, {
             headers:{Authorization: `Bearer ${token}`},
             method: "DELETE"
            })
